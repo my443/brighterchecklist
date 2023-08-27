fc0deaebd0f740e708e05b2fa91567e4859ff1f4
John van Dijk <fortyfour-three@teksavvy.com>
2023-08-26 20:30:41 -0400

    Changed database connection to postgresql. ran run-syncdb to migrate the schema to postgres.

--- brighterchecklist/brighterchecklist/settings.py
+++ brighterchecklist/brighterchecklist/settings.py
@@ -77,13 +77,28 @@ WSGI_APPLICATION = 'brighterchecklist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'HOST': 'localhost',
+        'NAME': 'brighterchecklist2023',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+        # "OPTIONS": {
+        #     "passfile": ".my_pgpass",
+        # },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
