78bae50220189536bcfb4c43d706d77a1b9d3740
John van Dijk <fortyfour-three@teksavvy.com>
2023-08-30 10:56:22 -0400

    Set up static urls for the assets for the website app. Changed the options to be dynamic for database settings. (So that the settings can be different on the server.

--- brighterchecklist/brighterchecklist/settings.py
+++ brighterchecklist/brighterchecklist/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': pg_name,
         'USER': pg_user,
         'PASSWORD': pg_pass,
-        'OPTIONS': {'sslmode': 'require'},
+        'OPTIONS': pg_options,
     }
 }
 
