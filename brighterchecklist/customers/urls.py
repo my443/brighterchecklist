8493d648aa42cdaa087ef408032441465d3ebb7d
John van Dijk <fortyfour-three@teksavvy.com>
2023-09-08 06:56:36 -0400

    Password change form and view completed.

--- brighterchecklist/customers/urls.py
+++ brighterchecklist/customers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
+from . import views, views_change_password
 
 urlpatterns = [
     path('new/', views.new_customer_signup, name='new_customer'),
     path('save/', views.save_customer_sign_up, name='save_customer'),
     path('thankyou/', views.thankyou, name='thank_you'),
+
+    path('password/', views_change_password.change_password, name='change_password'),
 ]
---
+++ brighterchecklist/customers/views_change_password.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.shortcuts import redirect
+
+# https://www.programcreek.com/python/example/50650/django.contrib.auth.forms.PasswordChangeForm
+def change_password(request):
+    """View function for the user profile, profile.html."""
+    # Get the current user's user object
+    # user = request.user
+    # # Look-up the username in the database
+    # current_user_name = User.objects.get(username=user.username)
+    # current_user_avatar = UserProfile.objects.get(user=user.id)
+    # If ths is a POST, process it as a password update
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            # This is a VERY important step!
+            update_session_auth_hash(request, user)
+            # messages.success(request,
+            #                  'Your password was successfully updated!',
+            #                  extra_tags='alert-success')
+            return redirect('/manager')
+    else:
+        form = PasswordChangeForm(request.user)
+
+    return render(request, 'customers/password_change.html', {
+        'form': form,
+        'user': request.user,
+        # 'current_user': current_user_name,
+        # 'user_avatar': current_user_avatar
+    })
