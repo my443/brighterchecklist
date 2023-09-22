f5db219521cff16c35c9a560c4865c4d0b852a5b
John van Dijk <fortyfour-three@teksavvy.com>
2023-09-22 05:18:47 -0400

    Gives an safe error message to the customer if they try to sign up with an email that already exists in the system.

--- brighterchecklist/customers/views_customer_signup.py
+++ brighterchecklist/customers/views_customer_signup.py
@@ -7,10 +7,15 @@ from .email import sendemail_with_template
 from django.template import loader
 from django.http import HttpResponse
 import shared.random_password_generator as random_password_generator
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 def new_customer_signup(request) -> HttpResponse:
     template = loader.get_template('customers/customer_details.html')
+
+    if customer_already_exists(request):
+        return redirect('home')
 
     new_customer = create_new_customer(request.POST['emailAddress'])
     form = generate_new_customer_form(new_customer)
@@ -25,6 +30,15 @@ def create_new_customer(email_address: str) -> Customer:
     new_customer.save()
 
     return new_customer
+
+def customer_already_exists(request)-> bool:
+    """Find out if that email address already exists"""
+    if Customer.objects.filter(email=request.POST['emailAddress']).exists():
+        messages.error(request,
+                       'That email address already exists in our system.<br>Every account requires a unique email address.')
+        return True
+    else:
+        return False
 
 def generate_new_customer_form(new_customer: Customer):
     data = {'email': new_customer.email,
@@ -42,6 +56,7 @@ def save_customer_sign_up(request):
     customer.firstname = request.POST['firstname']
     customer.lastname = request.POST['lastname']
     customer.last_updated = datetime.datetime.now()
+
     customer.save()
 
     user = create_new_user_for_new_customers(customer.email)
