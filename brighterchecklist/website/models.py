4d59c59439131e2058ef3bf6637522b1471f6f68
John van Dijk <fortyfour-three@teksavvy.com>
2023-09-25 06:20:44 -0400

    Working on feedback form.

--- brighterchecklist/website/models.py
+++ brighterchecklist/website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class Feedback(models.Model):
+    feedback_text = models.TextField(null=True)
+    user_giving_feedback = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user_giving_feedback', null=True)
+    feedback_date_time = models.DateTimeField(default=datetime.datetime.now())
